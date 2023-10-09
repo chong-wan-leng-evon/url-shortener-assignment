7f8bccfbba7aac7c7872af7f7cc0b5b9c56e373b
evonchong <evon.chong@11-technology.io>
2023-10-09 11:43:22 +0800

    Install uniqid
    To generate 18 byte unique id's based on the time, process id and mac address. Works on multiple processes and machines.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const cors = require("cors");
 
 const app = express();
 
+const db = require("./app/models");
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+
 // frontend
 var corsOptions = {
   origin: "http://localhost:8081"
@@ -20,6 +25,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ message: "Tech Assignment - Build a URL Shortener" });
 });
+
+require("./app/routes/routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
