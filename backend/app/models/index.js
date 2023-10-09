169c9de400b3c591750b63ee7b8cf7c12f3623fa
evonchong <evon.chong@11-technology.io>
2023-10-09 11:27:07 +0800

    Initialize Sequelize

---
+++ backend/app/models/index.js
@@ -0,0 +1,17 @@
+// retrieve db settings from config
+const dbConfig = require("../config/db");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.urlShortener = require("./urlShortenerModel")(sequelize, Sequelize);
+module.exports = db;
