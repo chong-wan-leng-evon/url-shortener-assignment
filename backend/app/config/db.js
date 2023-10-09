8853afdc7096dd95d9b15b6daa7f0732bcda502c
evonchong <evon.chong@11-technology.io>
2023-10-09 11:25:50 +0800

    Configure MySQL database

---
+++ backend/app/config/db.js
@@ -0,0 +1,9 @@
+// configuration based on your mysql database - to change accordingly
+module.exports = {
+    HOST: "127.0.0.1",
+    PORT: "3306",
+    USER: "root",
+    PASSWORD: "!!nEg{OS__j4N7m,!,Q]",
+    DB: "url_shortener",
+    dialect: "mysql"
+  };
